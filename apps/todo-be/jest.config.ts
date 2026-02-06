9e7273966760973d9685c163b0351b1f6891be9b
Kateryna Skoryna <k.skoryna@gmail.com>
2026-02-06 17:06:35 +0100

    Update test configuration

--- apps/todo-be/jest.config.ts
+++ apps/todo-be/jest.config.ts
@@ -6,5 +6,6 @@ export default {
   transform: {
     "^.+\\.[tj]s$": ["ts-jest", {tsconfig: "<rootDir>/tsconfig.spec.json"}]
   },
-  moduleFileExtensions: ["ts", "js", "html"]
+  moduleFileExtensions: ["ts", "js", "html"],
+  setupFilesAfterEnv: ["<rootDir>/src/app/test-setup.ts"]
 };
