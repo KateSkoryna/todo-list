f203e7378965e80e89f673e5242ffcbf225b0587
Kateryna Skoryna <k.skoryna@gmail.com>
2026-02-06 11:34:08 +0100

    test tailwind in app file

--- apps/todo/src/app/app.tsx
+++ apps/todo/src/app/app.tsx
@@ -2,11 +2,14 @@ import Tasks from './tasks';
 
 export function App() {
   return (
-    <>
-      <h1>Your Tasks</h1>
-      <Tasks/>
-      <div/>
-    </>
+    <div className="min-h-screen bg-gray-100 py-8">
+      <div className="max-w-4xl mx-auto px-4">
+        <h1 className="text-4xl font-bold text-gray-900 mb-8">Your Tasks</h1>
+        <div className="bg-white rounded-lg shadow-md p-6">
+          <Tasks/>
+        </div>
+      </div>
+    </div>
   );
 }
 
