f7a0b76e4c5e8e7be2493f3bf6f009436e7edf43
Kateryna Skoryna <k.skoryna@gmail.com>
2026-02-06 17:33:02 +0100

    Update repositories

--- apps/todo-be/src/app/repositories/todo.repository.ts
+++ apps/todo-be/src/app/repositories/todo.repository.ts
@@ -1,23 +1,40 @@
 import { Todo } from '../models/todo.model';
+import { TodoItem } from '@fyltura/types';
 
 export const TodoRepository = {
-  findAll: async () => {
-    return await Todo.find();
+  findAll: async (): Promise<TodoItem[]> => {
+    const docs = await Todo.find();
+    return docs.map(doc => doc.toJSON()) as TodoItem[];
   },
 
-  findById: async (id: string) => {
-    return await Todo.findById(id);
+  findById: async (id: string): Promise<TodoItem | null> => {
+    const doc = await Todo.findById(id);
+    if (!doc) {
+      return null;
+    }
+    return doc.toJSON() as TodoItem;
   },
 
-  create: async (todolistId: string, name: string, isDone: boolean = false) => {
-    return await Todo.create({ todolistId, name, isDone });
+  create: async (todolistId: string, name: string, isDone: boolean = false): Promise<TodoItem> => {
+    const doc = await Todo.create({ todolistId, name, isDone });
+    return doc.toJSON() as TodoItem;
   },
 
-  update: async (id: string, name: string, isDone: boolean) => {
-    return await Todo.findByIdAndUpdate(id, { name, isDone }, { new: true });
+  update: async (id: string, updates: { name?: string; isDone?: boolean }): Promise<TodoItem | null> => {
+    const doc = await Todo.findByIdAndUpdate(id, updates, { new: true }).exec();
+    if (!doc) {
+      return null;
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (doc as any).toJSON() as TodoItem;
   },
 
-  delete: async (id: string) => {
-    return await Todo.findByIdAndDelete(id);
+  delete: async (id: string): Promise<TodoItem | null> => {
+    const doc = await Todo.findByIdAndDelete(id).exec();
+    if (!doc) {
+      return null;
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (doc as any).toJSON() as TodoItem;
   },
 };
