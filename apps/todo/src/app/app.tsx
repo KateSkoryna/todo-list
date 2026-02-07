3d737620b7fd57af41f535af3fc3791e63c3dc8d
Kateryna Skoryna <k.skoryna@gmail.com>
2026-02-07 13:35:10 +0100

    remove fallback from app

--- apps/todo/src/app/app.tsx
+++ apps/todo/src/app/app.tsx
@@ -1,13 +1,13 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import Container from './component/elements/Container';
 import Header from './component/elements/Header';
 import Dropdown from './component/elements/Dropdown';
 import TodoContainer from './component/todo/todoContainer';
-import ErrorFallback from './component/elements/ErrorFallback';
+import { useEffect } from 'react';
 
 function App() {
   const { userId } = useParams<{ userId: string }>();
-  const parsedUserId = userId ? parseInt(userId) : undefined;
+  const navigate = useNavigate();
 
   // Dummy user data for demonstration
   const users = [
@@ -17,8 +17,14 @@ function App() {
     { id: '4', name: 'David' },
     { id: '5', name: 'Eve' },
   ];
+  const user = users.find((u) => u.id === userId);
+  const parsedUserId = userId ? parseInt(userId) : undefined;
 
-  const existedUser = users.find((user) => user.id === userId);
+  useEffect(() => {
+    if (userId && !user) {
+      navigate('/');
+    }
+  }, [userId, user, navigate]);
 
   return (
     <div className="min-h-screen bg-base-bg py-8">
@@ -26,9 +32,6 @@ function App() {
         <Header />
         <Container className="space-y-6">
           <Dropdown users={users} />
-          {!existedUser && (
-            <ErrorFallback error={new Error('User not found')} />
-          )}
           {parsedUserId && users.some((user) => user.id === userId) && (
             <TodoContainer userId={parsedUserId} />
           )}
