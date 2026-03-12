2b5d7aaccd9718e258a22cc8532477208f26e766
Kateryna Skoryna <k.skoryna@gmail.com>
2026-03-12 14:21:53 +0100

    Add User mongoose model

---
+++ apps/todo-be/src/app/models/user.model.ts
@@ -0,0 +1,33 @@
+import { Schema, model, Document, Types } from 'mongoose';
+import { User } from '@fyltura/types';
+
+interface IUserDocument extends Omit<User, 'id'>, Document {
+  _id: Types.ObjectId;
+  passwordHash: string;
+}
+
+const userSchema = new Schema<IUserDocument>(
+  {
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    passwordHash: { type: String, required: true },
+    displayName: { type: String, required: true, trim: true },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (_, ret: IUserDocument) => {
+        const user: User = {
+          id: ret._id.toString(),
+          email: ret.email,
+          displayName: ret.displayName,
+          createdAt: ret.createdAt,
+          updatedAt: ret.updatedAt,
+        };
+        return user;
+      },
+    },
+    toObject: { virtuals: true },
+  }
+);
+
+export const UserModel = model<IUserDocument>('User', userSchema);
