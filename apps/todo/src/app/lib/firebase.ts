806ad66ad28fcadabc0132c3aa5affa6097dacf0
Kateryna Skoryna <k.skoryna@gmail.com>
2026-04-14 14:54:13 +0200

    set up Firebase Storage with emulator

--- apps/todo/src/app/lib/firebase.ts
+++ apps/todo/src/app/lib/firebase.ts
@@ -4,13 +4,16 @@ import {
   GoogleAuthProvider,
   connectAuthEmulator,
 } from 'firebase/auth';
+import { getStorage, connectStorageEmulator } from 'firebase/storage';
 import { environment } from '../../environments/environment';
 
 const firebaseApp = initializeApp(environment.firebase);
 
 export const auth = getAuth(firebaseApp);
 export const googleProvider = new GoogleAuthProvider();
+export const storage = getStorage(firebaseApp);
 
 if (!environment.production) {
   connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
+  connectStorageEmulator(storage, 'localhost', 9199);
 }
